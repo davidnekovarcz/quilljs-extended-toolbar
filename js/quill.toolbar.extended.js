f3f0f39b9b86ec0dfc3bce9db2a3982f0187517b
Dave Nekovar <nekovar.david@gmail.com>
2017-02-22 14:51:36 +0100

    add basic table implementation, use core from dist folder

--- js/quill.toolbar.extended.js
+++ js/quill.toolbar.extended.js
@@ -1,11 +1,179 @@
+let Inline = Quill.import('blots/inline');
+let Block = Quill.import('blots/block');
+let Delta = Quill.import('delta');
+let BlockEmbed = Quill.import('blots/block/embed');
+
+//
+//
+// TABLE TAG
+//
+
+class StaticInline extends Inline {
+  // static formats(node) {
+  //   console.log('inline formats');
+  // }
+  //
+  // delta() {
+  //   console.log('inline delta');
+  // }
+  //
+  // format(name, value) {
+  //   console.log('table format');
+  // }
+  //
+  // formatAt(index, length, name, value) {
+  //   console.log('table formatAt');
+  // }
+  //
+  // insertAt(index, value, def) {
+  //   console.log('table insertAt');
+  // }
+  //
+  // optimize() {
+  //   console.log('inline optimize');
+  // }
+  //
+  // replace(target) {
+  //   console.log('inline replace');
+  // }
+  //
+  // remove() {
+  //   console.log('inline remove');
+  // }
+}
+Quill.register(StaticInline);
+
+class Td extends StaticInline {}
+
+Td.blotName = 'td';
+Td.tagName = 'td';
+Quill.register(Td);
+
+class Tr extends StaticInline {}
+
+Tr.blotName = 'tr';
+Tr.tagName = 'tr';
+Quill.register(Tr);
+
+class Tbody extends StaticInline {}
+
+Tbody.blotName = 'tbody';
+Tbody.tagName = 'tbody';
+Quill.register(Tbody);
+
+class Table extends BlockEmbed {
+  static create(value) {
+    let node = super.create(value);
+
+    if(value === true || value.includes('newtable_')) {
+      node = this.generate_empty_table(value, node);
+    } else if(typeof value === 'string' || value.includes('td')) {
+      node.innerHTML = value;
+    }
+
+    return node;
+  }
+
+  static generate_empty_table(value, node) {
+    let cols = 5;
+    let rows = 5;
+    if(typeof value === 'string' && value.includes('newtable_')) {
+      value = value.split('_');
+      rows = value[1];
+      cols = value[2];
+    } else {
+      rows = prompt('Number of rows?', cols);
+      cols = prompt('Number of cols?', cols);
+    }
+    value = [];
+    for (let r = 0; r < rows; r++) {
+      value[r] = [];
+      for (let c = 0; c < cols; c++) {
+        value[r].push("");
+      }
+    }
+    var tbody = document.createElement('tbody');
+    value.forEach(row => {
+      var tr = document.createElement('tr');
+      tbody.appendChild(tr);
+      row.forEach(cell => {
+        let td = document.createElement('td');
+        td.innerText = cell;
+        tr.appendChild(td);
+      })
+    })
+    node.appendChild(tbody);
+    return node;
+  }
+
+  static value(node) {
+    console.log('table value');
+    var ret = [];
+    if(node.tagName === 'table') {
+      let rows = node.rows;
+      for (let i = 0; i < rows.length; i++) {
+        ret[i] = [];
+        for (let j = 0; j < rows[i].cells.length; j++) {
+          ret[i].push(rows[i].cells[j].innerText);
+        }
+      }
+    }
+    return ret;
+  }
+
+  static formats(node) {
+    console.log('table formats');
+    // super.formats(node);
+  }
+
+  delta() {
+    // console.log('table delta');
+    let d = new Delta().insert({ table: this.domNode.innerHTML });
+    return d;
+  }
+
+  // format(name, value) {
+  //   console.log('table format');
+  //   // super.format(name, value);
+  // }
+
+  // formatAt(index, length, name, value) {
+  //   console.log('table formatAt');
+  //   // super.formatAt(index, length, name, value);
+  // }
+
+  insertAt(index, value, def) {
+    console.log('table insertAt');
+    // super.insertAt(index, value, def);
+  }
+
+  // optimize() {
+  //   console.log('table optimize');
+  //   super.optimize();
+  // }
+
+  replace(target) {
+    console.log('table replace');
+    super.replace(target);
+  }
+
+  remove() {
+    console.log('table remove');
+    var remove_table = confirm('Are you sure you want to remove this table?');
+    if (remove_table) super.remove();
+  }
+}
+
+Table.blotName = 'table';
+Table.tagName = 'table';
+Quill.register(Table);
+
 //
 //
 // PLACEHOLDER TAG
 //
 
 const placeholder_tag = 'x-placeholder'
-
-let Inline = Quill.import('blots/inline');
 
 class PlaceholderBlot extends Inline { }
 
@@ -19,8 +187,6 @@ Quill.register(PlaceholderBlot);
 //
 
 const if_tag = 'x-if'
-
-let Block = Quill.import('blots/block');
 
 class IfBlot extends Block {
   static create(value) {
@@ -37,74 +203,6 @@ class IfBlot extends Block {
 IfBlot.blotName = 'if';
 IfBlot.tagName = 'x-if';
 Quill.register(IfBlot);
-
-//
-//
-// TABLE TAG
-//
-
-let BlockEmbed = Quill.import('blots/block');
-
-class TbodyBlot extends Inline { }
-
-TbodyBlot.blotName = 'tbody';
-TbodyBlot.tagName = 'tbody';
-Quill.register(TbodyBlot);
-
-class TrBlot extends Inline { }
-
-TrBlot.blotName = 'tr';
-TrBlot.tagName = 'tr';
-Quill.register(TrBlot);
-
-class TdBlot extends Inline { }
-
-TdBlot.blotName = 'td';
-TdBlot.tagName = 'td';
-Quill.register(TdBlot);
-
-class TableBlot extends BlockEmbed {
-  static create(value) {
-    if(!Array.isArray(value)) {
-      value = [
-        ["Column A", "Column B", "Column C"],
-        ["Row 1", "Row 2", "Row 3"]
-      ]
-    }
-    let node = super.create(value);
-    let tbody = document.createElement('tbody');
-    node.appendChild(tbody);
-
-    value.forEach(row => {
-      var tr = document.createElement('tr');
-      tbody.appendChild(tr);
-      row.forEach(cell => {
-        let td = document.createElement('td');
-        td.innerText = cell;
-        tr.appendChild(td);
-      })
-    })
-    return node;
-  }
-
-  static value(node) {
-    var ret = [];
-    if(node.tagName === 'table') {
-      let rows = node.rows;
-      for (let i = 0; i < rows.length; i++) {
-        ret[i] = [];
-        for (let j = 0; j < rows[i].cells.length; j++) {
-          ret[i].push(rows[i].cells[j].innerText);
-        }
-      }
-    }
-    return ret;
-  }
-}
-
-TableBlot.blotName = 'table';
-TableBlot.tagName = 'table';
-Quill.register(TableBlot);
 
 //
 //
